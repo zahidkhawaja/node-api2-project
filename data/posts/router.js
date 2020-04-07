c181b95b17e9a1b3e7127cd9e15d145f7f464ddb
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-07 17:45:28 -0400

    Add endpoint to add a post

--- data/posts/router.js
+++ data/posts/router.js
@@ -17,4 +17,18 @@ router.get("/", (req, res) => {
       });
   });
 
+  router.post("/", (req, res) => {
+    console.log(req.body)
+    const { title, contents } = req.body;
+    !title || !contents ? res.status(400).json({ message: "Title or contents missing"}) : 
+    Posts.insert(req.body).then(() => {
+      res.status(201).json(req.body);
+    })
+    .catch(() =>{
+        res.status(500).json({message: "Server error, cannot post"})
+    })
+});
+
+
+
   module.exports = router;
