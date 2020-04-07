ab54be22dbf8f6d6aa9ef583f93bee35e17d786a
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-07 16:48:30 -0400

    Setup initial route for posts

---
+++ data/posts/router.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const Posts = require("./01-posts");
+
+const router = express.Router();
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
+const postsRouter = require("./data/posts/router");
+
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/posts", postsRouter); 
 
 server.get("/", (req, res) => {
   res.send(`
